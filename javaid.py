300b33d31ab5305cbad80127987bffe39382cc29
Jianwei Huang <cloudease@hotmail.com>
2018-10-21 00:32:12 +0800

    The output now contains class & inner class & method name

--- javaid.py
+++ javaid.py
@@ -72,20 +72,33 @@ class javaid(object):
         except:
             raise
 
-    def getClassName(self, filepath):
+    def getJAVAPath(self, filepath):
         try:
             pattern = re.compile(r'.*/src/main/java/')
             tmpClassName = re.sub(pattern, '', filepath)
-            return tmpClassName.replace("/", ".")
+            tmpClassName = tmpClassName.replace("/", ".")
+
+            tmpjavaPath = re.sub('\\.[^\\.]*\\.java', '',tmpClassName)
+
+            #print ("debug --- %s -- %s -- %s" % (tmpClassName, tmpjavaPath, str(re.search(file_pattern, tmpClassName))))
+            if self.inner_class == '':
+                javaPath = tmpjavaPath + "." + self.cur_class
+            else:
+                javaPath = tmpjavaPath + "." + self.cur_class + "." + self.inner_class
+
+            return javaPath
         except:
             return filepath
 
-    def report_id(self,vul):
-        self.cur_id = self._filename
+    # def report_id(self,vul):
+    #     self.cur_id = self._filename
         # print "[+]【"+vul+"】identify danger function ["+self._function+"] in file "+self._filename,
 
     def report_line(self):
-        print "[" + self._function + "]  " + self.getClassName(self._filename) + "@"+ str(self._line)
+        #print "[" + self._function + "]  " + self.getJAVAPath(self._filename) + "@"+ str(self._line)
+        javaPath = self.getJAVAPath(self._filename)
+        #print ("[%s] class name: %s, method name: %s, inner class: %s, java path: %s" % (self._function, self.cur_class, self.cur_method, self.inner_class, javaPath))
+        print ( "[%s] %s#%s@%s" %(self._function, javaPath, self.cur_method, self._line))
 
     def handlePath(self, path):
         dirs = os.listdir(path) 
@@ -112,9 +125,23 @@ class javaid(object):
         #print '------------------------'
 
     def function_search_line(self):
+        methodFullPattern = re.compile('\\s{0,}(public|private|protected)\\s{0,}(static|synchronized|final|\s){0,}\\s{0,}([a-zA-Z0-9<>\\.,]){1,}\\s{0,}([a-zA-Z0-9]){1,}\\s?\\(')
+        methodPreffixPattern = re.compile('\\s{0,}(public|private|protected)\\s{0,}(static|synchronized|final|\s){0,}\\s{0,}([\\w<>,\\.\\[\\]]+)\\s{0,}')
+
+        classFullPattern = re.compile('\\s{0,}(public|private|protected)\\s{0,}(static|synchronized|final){0,}\\s{0,}(class)\\s{0,}([a-zA-Z0-9]){1,}')
+        classPreffixPattern = re.compile('\\s{0,}(public|private|protected)\\s{0,}(static|synchronized|final){0,}\\s{0,}(class)\\s{0,}')
+
+        self.cur_method = ''
+        self.cur_class = ''
+        self.inner_class = ''
+
         fl = open(self._filename, 'r') 
-        self._line =0
-        importregexp="import\s[^;]*;"
+        self._line = 0
+
+        importregexp = "import\s[^;]*;"
+        exp_pattern = re.compile(importregexp)
+
+
         #print "function_search_line"+self._filename
         while True:
             line = fl.readline() 
@@ -123,14 +150,32 @@ class javaid(object):
                 break
 
             self._line += 1
-            #print line
-            exp_pattern = re.compile(importregexp)
+
             if exp_pattern.search(line):
                 continue
+
+            # match class name or inner class name
+            if re.match(classFullPattern, line):
+                tmpClassName = re.search(classFullPattern, line).group(0)
+
+                if self.cur_class == '':
+                    #print "debug --- " + str(re.search(classFullPattern, line).group(0)) + '   ---   ' + line
+                    self.cur_class = re.sub(classPreffixPattern, '', tmpClassName)
+                else:
+                    self.inner_class = re.sub(classPreffixPattern, '', tmpClassName)
+
+                continue
+
+            # match method name
+            if re.match(methodFullPattern, line):
+                tmpMethodName = re.search(methodFullPattern, line).group(0)
+                self.cur_method = re.sub(methodPreffixPattern, '', tmpMethodName)[:-1]
+
             if self._function in line:
                 #print 'find danger function on line :' + str(line)
                 self.report_line()
                 continue
+
         fl.close()
 
     def regexp_search(self,rule_dom,content):
@@ -141,7 +186,9 @@ class javaid(object):
                 exp_pattern = re.compile(regexp_dom.text)
                 if exp_pattern.search(content):
                     #print "identify sfunction is : "+self._function
-                    self.report_id(self._vultype)
+                    self.cur_id = self._filename
+
+                    #self.report_id(self._vultype)
                     self.function_search_line()
 
         return True
